303e90011fbbd918dc753b8fc5d7f92c012d6281
Runy <87627937+Ruunie@users.noreply.github.com>
2023-04-17 17:33:54 +0900

    회원가입시, 입력데이터 유효성 체크

--- src/main/java/com/kmj/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kmj/exam/demo/controller/UsrMemberController.java
@@ -12,25 +12,51 @@ import com.kmj.exam.demo.vo.Member;
 @Controller
 public class UsrMemberController {
 	private MemberService memberService;
-	
+
 	public UsrMemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
-	
+
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+			String email) {
+
+		if (loginId == null || loginId.trim().length() == 0) {
+			return "아이디를 입력해주세요.";
+		}
+
+		if (loginPw == null || loginPw.trim().length() == 0) {
+			return "비밀번호를 입력해주세요.";
+		}
+
+		if (name == null || name.trim().length() == 0) {
+			return "이름을 입력해주세요.";
+		}
+
+		if (nickname == null || nickname.trim().length() == 0) {
+			return "닉네임을 입력해주세요.";
+		}
+
+		if (cellphoneNo == null || cellphoneNo.trim().length() == 0) {
+			return "전화번호를 입력해주세요.";
+		}
+
+		if (email == null || email.trim().length() == 0) {
+			return "이메일을 입력해주세요.";
+		}
+
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		
+
 		if (id == -1) {
 			return "해당 로그인 아이디는 이미 사용중입니다.";
 		}
-		
+
 		Member member = memberService.getMemberById(id);
-		
+
 		return member;
 	}
-	
+
 	@RequestMapping("/usr/member/getMembers")
 	@ResponseBody
 	public List<Member> getMembers() {
