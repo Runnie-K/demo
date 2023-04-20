6011424ccd98aa298f87da4b6354f4e6c53b55f6
Runy <87627937+Ruunie@users.noreply.github.com>
2023-04-20 16:03:29 +0900

    출력용 게시물 가져올때, 액터정보 넘겨서 더욱 자세한 정보 생성하기

--- src/main/java/com/khj/exam/demo/service/ArticleService.java
+++ src/main/java/com/khj/exam/demo/service/ArticleService.java
@@ -12,47 +12,79 @@ import com.khj.exam.demo.vo.ResultData;
 @Service
 public class ArticleService {
 	private ArticleRepository articleRepository;
-	
+
 	public ArticleService(ArticleRepository articleRepository) {
 		this.articleRepository = articleRepository;
 	}
-	
-	public Article getForPrintArticle(int id) {
+
+	public Article getForPrintArticle(int id, int actorId) {
+		Article article = articleRepository.getForPrintArticle(id);
+
+		updateForPrintData(actorId, article);
+
 		return articleRepository.getForPrintArticle(id);
 	}
-	
-	public List<Article> getForPrintArticles() {
+
+	public List<Article> getForPrintArticles(int actorId) {
+		List<Article> articles = articleRepository.getForPrintArticles();
+
+		for (Article article : articles) {
+			updateForPrintData(actorId, article);
+		}
+
 		return articleRepository.getForPrintArticles();
 	}
+
+	private void updateForPrintData(int actorId, Article article) {
+		if (article == null) {
+			return;
+		}
+		
+		ResultData actorCanDeleteRd = actorCanDelete(actorId, article);
+		article.setExtra__actorCanDelete(actorCanDeleteRd.isSuccess());
+
+	}
 	
+	public ResultData actorCanDelete(int actorId, Article article) {
+		if (article == null) {
+			return ResultData.from("F-1", "게시물이 존재하지 않습니다.");
+		}
+		
+		if (article.getMemberId() != actorId) {
+			return ResultData.from("F-1", "권한이 없습니다.");
+		}
+		
+		return ResultData.from("S-1", "게시물 삭제가 가능합니다.");
+	}
+
 	public ResultData<Integer> writeArticle(int memberId, String title, String body) {
 		articleRepository.writeArticle(memberId, title, body);
 		int id = articleRepository.getLastInsertId();
-		
+
 		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), "id", id);
 	}
-	
+
 	public void deleteArticle(int id) {
 		articleRepository.deleteArticle(id);
 	}
 
 	public ResultData<Article> modifyArticle(int id, String title, String body) {
 		articleRepository.modifyArticle(id, title, body);
-		
-		Article article = getForPrintArticle(id);
-		
+
+		Article article = getForPrintArticle(0, id);
+
 		return ResultData.from("S-1", Ut.f("%d번 게시물이 수정되었습니다.", id), "article", article);
 	}
 
 	public ResultData actorCanModify(int actorId, Article article) {
-		if ( article == null ) {
+		if (article == null) {
 			return ResultData.from("F-1", "권한이 없습니다.");
 		}
-		
-		if ( article.getMemberId() != actorId ) {
+
+		if (article.getMemberId() != actorId) {
 			return ResultData.from("F-2", "권한이 없습니다.");
 		}
-		
+
 		return ResultData.from("S-1", "수정가능합니다.");
 	}
 }
