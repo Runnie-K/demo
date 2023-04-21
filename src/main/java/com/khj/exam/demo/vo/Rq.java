5e760908788d944c2eb4808951f37885c8b296de
Runy <87627937+Ruunie@users.noreply.github.com>
2023-04-21 14:57:05 +0900

    로그인 처리에 Rq 적용

--- src/main/java/com/khj/exam/demo/vo/Rq.java
+++ src/main/java/com/khj/exam/demo/vo/Rq.java
@@ -14,42 +14,43 @@ public class Rq {
 	private boolean isLogined;
 	@Getter
 	private int loginedMemberId;
-
+	
 	private HttpServletRequest req;
 	private HttpServletResponse resp;
-
+	private HttpSession session;
+	
 	public Rq(HttpServletRequest req, HttpServletResponse resp) {
 		this.req = req;
 		this.resp = resp;
-
-		HttpSession httpSession = req.getSession();
-
+		
+		this.session = req.getSession();
+		
 		boolean isLogined = false;
 		int loginedMemberId = 0;
-
-		if (httpSession.getAttribute("loginedMemberId") != null) {
+		
+		if ( session.getAttribute("loginedMemberId") != null) {
 			isLogined = true;
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
 		}
-
+		
 		this.isLogined = isLogined;
 		this.loginedMemberId = loginedMemberId;
 	}
-
+	
 	public void printHistoryBackJs(String msg) {
 		resp.setContentType("text/html; charset=UTF-8");
-
+		
 		println("<script>");
-
+		
 		if (!Ut.empty(msg)) {
 			println("alert('" + msg + "');");
 		}
-
+		
 		println("history.back();");
-
+		
 		println("</script>");
 	}
-
+	
 	public void print(String str) {
 		try {
 			resp.getWriter().append(str);
@@ -58,9 +59,13 @@ public class Rq {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public void println(String str) {
 		print(str + "\n");
 	}
 
+	public void login(Member member) {
+		session.setAttribute("loginedMemberId", member.getId());
+	}
+	
 }
